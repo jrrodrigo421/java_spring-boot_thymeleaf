a7844803a0dd34fe8f8ee928f19ad7a22f28d703
rodrigo <rjr89000@gmail.com>
2023-07-02 16:32:02 -0300

    Service Layer implementation

--- src/main/java/com/example/demospring/service/FuncionarioServiceImpl.java
+++ src/main/java/com/example/demospring/service/FuncionarioServiceImpl.java
@@ -4,25 +4,30 @@ import com.example.demospring.dao.FuncionarioDao;
 import com.example.demospring.domain.Funcionario;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Service
+@Transactional(readOnly = true)
 public class FuncionarioServiceImpl implements FuncionarioService{
 
     @Autowired
     private FuncionarioDao dao;
     @Override
+    @Transactional(readOnly = false)
     public void salvar(Funcionario funcionario) {
         dao.save(funcionario);
     }
 
     @Override
+    @Transactional(readOnly = false)
     public void editar(Funcionario funcionario) {
         dao.update(funcionario);
     }
 
     @Override
+    @Transactional(readOnly = false)
     public void excluir(Long id) {
         dao.delete(id);
     }
