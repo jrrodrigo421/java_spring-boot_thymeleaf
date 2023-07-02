5e77ebc8c4ae018e164b23fa55cdb9b2494ecdd3
rodrigo <rjr89000@gmail.com>
2023-07-02 17:38:30 -0300

    fragments thymeleaf

--- src/main/java/com/example/demospring/DemoSpringApplication.java
+++ src/main/java/com/example/demospring/DemoSpringApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemospringApplication {
+public class DemoSpringApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemospringApplication.class, args);
+		SpringApplication.run(DemoSpringApplication.class, args);
 	}
 
 }
