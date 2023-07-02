dfa2a98578b0ce45fa40697880593dc7c57158a4
rodrigo <rjr89000@gmail.com>
2023-07-02 18:05:58 -0300

    fragments thymeleaf and layout

--- src/main/java/com/example/demospring/dao/FuncionarioDaoImpl.java
+++ src/main/java/com/example/demospring/dao/FuncionarioDaoImpl.java
@@ -1,7 +1,9 @@
 package com.example.demospring.dao;
 
 import com.example.demospring.domain.Funcionario;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public class FuncionarioDaoImpl extends AbstractDao<Funcionario, Long> implements FuncionarioDao {
 
 }
