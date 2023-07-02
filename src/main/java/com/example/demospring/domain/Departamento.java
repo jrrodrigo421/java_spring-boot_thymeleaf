7bec83ef4d75c4f04544e063cfc55e6e62ac3132
rodrigo <rjr89000@gmail.com>
2023-07-02 13:22:53 -0300

    adding entites and configuring database

---
+++ src/main/java/com/example/demospring/domain/Departamento.java
@@ -0,0 +1,33 @@
+package com.example.demospring.domain;
+
+import java.util.List;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "DEPARTAMENTOS")
+public class Departamento extends AbstractEntity<Long> {
+
+  @Column(name = "nome", nullable = false, unique = true, length = 60)
+  private String nome;
+
+  @OneToMany(mappedBy = "departamento")
+  private List<Cargo> cargos;
+
+  public List<Cargo> getCargos() {
+    return cargos;
+  }
+
+  public void setCargos(List<Cargo> cargos) {
+    this.cargos = cargos;
+  }
+
+  public String getNome() {
+    return nome;
+  }
+
+  public void setNome(String nome) {
+    this.nome = nome;
+  }
+
+}
